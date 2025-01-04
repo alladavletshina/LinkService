df137fc03bf5b534a222c6feab1ce4d8cfd1b8f1
Davletshina Alla <atdavletshina@gmail.com>
2025-01-04 23:48:47 +0300

    04012025

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,50 @@
 package org.example;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     private final Scanner scanner;
-    private final UrlShortMaker urlShortMaker;
+    private String userId;
 
     public Main() {
         scanner = new Scanner(System.in);
-        urlShortMaker = new UrlShortMaker();
     }
 
-    public void startApplication() {
+    private void startApplication(String userId) {
         while (true) {
             showMenu();
             int choice = getUserChoice();
+            UrlShortMaker urlShortMaker = new UrlShortMaker();
+            String shortUrl;
+            this.userId = userId;
 
             switch (choice) {
                 case 1:
                     System.out.println("Введите длинный URL для преобразования в короткую ссылку");
-                    //String longId = scanner.nextLine();
-                    String longId = "https://www.baeldung.com/java-9-http-clients";
-                    System.out.println(longId);
-                    //String shortUrl = urlShortMaker.linkShortMaker(longId);
-                    String shortUrl = urlShortMaker.generateUniqueUrl(longId);
+                    String longUrl = "https://www.baeldung.com/java-9-http-clients";
+                    System.out.println(longUrl);
+
+                    System.out.println("Установите лимит на количество переходов по ссылке");
+                    int maxClicks = getUserChoice();
+
+                    shortUrl = urlShortMaker.genеrateShortUrl(userId,maxClicks, longUrl);
                     System.out.println("Короткая ссылка: " + shortUrl);
+                    urlShortMaker.printHistory();
                     break;
                 case 2:
-                    System.out.println("КОД-2");
+                    System.out.println("Введите короткую ссылку: ");
+                    shortUrl = scanner.nextLine();
+                    userId = scanner.nextLine();
+                    System.out.println(urlShortMaker.getLongUrl(userId, shortUrl));
+                    urlShortMaker.printHistory();
                     break;
                 case 3:
                     System.out.println("КОД-3");
                     break;
                 case 0:
-                    exitApp();
                     return;
+                default:
+                    System.out.println("Неправильный выбор. Попробуйте снова.");
             }
         }
     }
@@ -49,9 +58,9 @@ public class Main {
     private void showMenu() {
         System.out.println("\nВыберите нужное действие:");
         System.out.println("1. Создать новую короткую ссылку");
-        System.out.println("2. Посмотреть список своих ссылок");
+        System.out.println("2. Перейти по короткой ссылке");
         System.out.println("3. Удалить свою ссылку");
-        System.out.println("0. Выход");
+        System.out.println("0. Вернуться в главное меню");
         System.out.print("Ваш выбор: ");
     }
 
@@ -64,9 +73,47 @@ public class Main {
         }
     }
 
+    private void greetingShowMenu() {
+        System.out.println("\nГлавное меню:");
+        System.out.println("1. У меня есть уникальный идентификационный номер");
+        System.out.println("2. Нет уникального идентификационного номера");
+        System.out.println("0. Выход из приложения");
+        System.out.print("Ваш выбор: ");
+    }
+
+    private void greetingApplication() {
+        Main main = new Main();
+        while (true) {
+            greetingShowMenu();
+            int choice = getUserChoice();
+            String userId;
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Введите уникальный идентификационный номер:");
+                    userId = scanner.nextLine();
+                    startApplication(userId);
+                    break;
+                case 2:
+                    User user = new User();
+                    userId = user.getUuid();
+                    System.out.println(userId);
+                    startApplication(userId);
+                    break;
+                case 0:
+                    exitApp();
+                    return;
+                default:
+                    System.out.println("Неправильный выбор. Попробуйте снова.");
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println("Добро пожаловать в сервис сокращения ссылок!");
-        main.startApplication();
+        main.greetingApplication();
         }
 }
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,16 @@
+package org.example;
+import java.util.UUID;
+
+public class User {
+
+    private String uuid;
+
+    public User() {
+        this.uuid = UUID.randomUUID().toString();
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+}
